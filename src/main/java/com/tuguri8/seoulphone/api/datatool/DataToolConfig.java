9b9e8e96468d86db453b7cac81b020a3371ffd1b
한승우 <tuguri8@gmail.com>
2019-09-28 20:10:35 +0900

    #1 휴대폰 찾기, 분실물 찾기 오픈API 연동

--- src/main/java/com/tuguri8/seoulphone/api/datatool/DataToolConfig.java
+++ src/main/java/com/tuguri8/seoulphone/api/datatool/DataToolConfig.java
@@ -1,4 +1,29 @@
 package com.tuguri8.seoulphone.api.datatool;
 
+import com.tuguri8.seoulphone.api.datatool.opendata.OpenDataClient;
+import feign.Feign;
+import feign.Retryer;
+import feign.jaxb.JAXBContextFactory;
+import feign.jaxb.JAXBDecoder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.apache.commons.lang.CharEncoding.UTF_8;
+
+@EnableFeignClients
+@Configuration
 public class DataToolConfig {
+
+    @Bean
+    public OpenDataClient openDataClient() {
+        return Feign.builder()
+                    .decoder(new JAXBDecoder(new JAXBContextFactory.Builder()
+                                                 .withMarshallerJAXBEncoding(UTF_8)
+                                                 .build()))
+                    .contract(new SpringMvcContract())
+                    .retryer(new Retryer.Default())
+                    .target(OpenDataClient.class, "opendata-client");
+    }
 }
